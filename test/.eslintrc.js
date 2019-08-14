436c08a3847f5ddeb0696fbaf0f736774c6561ac
Oanh Nguyen <oanhnn.bk@gmail.com>
2019-08-14 14:13:51 +0700

    Add test

---
+++ test/.eslintrc.js
@@ -0,0 +1,17 @@
+export default {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: '2017'
+  },
+  rules: {
+    'no-console': 'off',
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
+};
